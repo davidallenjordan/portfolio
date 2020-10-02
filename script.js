f4a06bdcc39c97d7f09854af44b5a2fe0ef225a5
David Allen-Jordan <davidallenjordan@gmail.com>
2020-10-02 19:10:56 -0400

    Removed section dividers, added gradients to header, updated fonts, changed button styles

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-// About page, heading and body slide in from left (maybe button too)
-// change html to point to mobile image path
-
-
-
-// Download Resume Button
-
-// Stretch Goals:
-// TypeWriter Effect for Web Developer
-
-
-
-// mobile nav.
-// on click of li with <a>, reveal (slide?) other li elements
 
 // App Name Space
 const app = () => {}
-
 
 // Mobile Nav Menu
 app.mobileNav = () => {
   $('.mobileNav').on('click', () => {
     $('.mobileDropDown').toggleClass('toggleDropDown');
   })
+}
+
+// Type Writer Effect ----->
+
+app.typeWriterEffect = () => {
+
 }
 
 // About Me slide in from left animation, CSS in _animations
